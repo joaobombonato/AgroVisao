5d9de1dabfb94a53b4ceb89e3587eb72be1768cf
jaobombonato <jaobombonato@gmail.com>
2026-02-27 18:39:17 -0300

    style: update visual version to v4.7.85

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@
 // CONSTANTES GLOBAIS
 // ==========================================
 
-export const APP_VERSION = "v4.7.84";
+export const APP_VERSION = "v4.7.85";
 
 export const DADOS_INICIAIS = {
   chuvas: [],
