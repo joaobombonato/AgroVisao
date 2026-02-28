eaa4373b352424359235912dea2811aee76a5c35
jaobombonato <jaobombonato@gmail.com>
2026-02-27 23:00:40 -0300

    chore: atualiza versao para v4.7.86 em constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@
 // CONSTANTES GLOBAIS
 // ==========================================
 
-export const APP_VERSION = "v4.7.85";
+export const APP_VERSION = "v4.7.86";
 
 export const DADOS_INICIAIS = {
   chuvas: [],
