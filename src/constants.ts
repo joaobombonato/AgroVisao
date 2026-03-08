76c536275f50bdbf5472dd0c444e33dc20c04586
jaobombonato <jaobombonato@gmail.com>
2026-03-08 02:30:01 -0300

    chore: atualiza versão para v4.7.104 e garante deploy das travas de segurança

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@
 // CONSTANTES GLOBAIS
 // ==========================================
 
-export const APP_VERSION = "v4.7.103";
+export const APP_VERSION = "v4.7.104";
 
 export const DADOS_INICIAIS = {
   chuvas: [],
