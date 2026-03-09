63cd471bf2854466dbf5353c95677a4db00d1379
jaobombonato <jaobombonato@gmail.com>
2026-03-09 11:17:53 -0300

    feat: implementar cadastro total de perfil e trava de segurança (v107)

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@
 // CONSTANTES GLOBAIS
 // ==========================================
 
-export const APP_VERSION = "v4.7.106";
+export const APP_VERSION = "v4.7.107";
 
 export const DADOS_INICIAIS = {
   chuvas: [],
